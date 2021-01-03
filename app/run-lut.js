1fb9b899187e6f5a2520af5846c345067f298102
Felix Akkermans <me+code@felixakkermans.com>
2021-01-03 14:13:12 +0100

    Add some more runs to LUT

--- app/run-lut.js
+++ app/run-lut.js
@@ -69,40 +69,72 @@ export const RUN_ADDRESS_LUT = {
       car: 85,
       transit: 190
     }
+  },
+  11: {
+    address: 'Outdoor Challenge Park, Bornsedijk 105, 7576 PG Oldenzaal',
+    travelTime: {
+      car: 140,
+      transit: 570
+    }
+  },
+  12: {
+    address: 'Hommes Survival & Outdoor Survivalbaan Zernike, De Bunders 2, 9747 AC Groningen',
+    travelTime: {
+      car: 160,
+      transit: 225
+    }
+  },
+  13: {
+    address: 'Trainingsgroep Survival Vollenhove',
+    travelTime: {
+      car: 120,
+      transit: 200
+    }
+  },
+  14: {
+    address: 'Survivalrun Meetjesland, Moerwege 57B, 9981 Maldegem, Belgium',
+    travelTime: {
+      car: 110,
+      transit: 210
+    }
+  },
+  15: {
+    address: 'Hang-On Run, Hydeparklaan 22-28, 3941 ZJ Doorn',
+    travelTime: {
+      car: 70,
+      transit: 135
+    }
+  },
+
+
+  21: {
+    address: 'Thor Run Zwolle, Weteringkade 1, 8031 PB Zwolle',
+    travelTime: {
+      car: 105,
+      transit: 150
+    }
+  },
+  22: {
+    address: 'Master van Dijkpaad 4, 8414 NB Nieuwehorne',
+    travelTime: {
+      car: 130,
+      transit: 240
+    }
+  },
+  23: {
+    address: 'Goudtsjeblomstrjitte 34-a, 9288 AM Kootstertille',
+    travelTime: {
+      car: 141,
+      transit: 240
+    }
+  },
+
+
+  34: {
+    address: 'Uppsalalaan 3, 3584 CT Utrecht',
+    travelTime: {
+      car: 60,
+      transit: 100
+    }
   }
-  // 11: {
-  //   address: 'Outdoor Challenge Park, Bornsedijk 105, 7576 PG Oldenzaal',
-  //   travelTime: {
-  //     car: null,
-  //     transit: null
-  //   }
-  // },
-  // 12: {
-  //   address: 'Hommes Survival & Outdoor Survivalbaan Zernike, De Bunders 2, 9747 AC Groningen',
-  //   travelTime: {
-  //     car: null,
-  //     transit: null
-  //   }
-  // },
-  // 13: {
-  //   address: 'Trainingsgroep Survival Vollenhove',
-  //   travelTime: {
-  //     car: null,
-  //     transit: null
-  //   }
-  // },
-  // 14: {
-  //   address: 'Survivalrun Meetjesland, Moerwege 57B, 9981 Maldegem, Belgium',
-  //   travelTime: {
-  //     car: null,
-  //     transit: null
-  //   }
-  // },
-  // 15: {
-  //   address: 'Hang-On Run, Hydeparklaan 22-28, 3941 ZJ Doorn',
-  //   travelTime: {
-  //     car: null,
-  //     transit: null
-  //   }
-  // }
 };
