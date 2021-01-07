a679323c49ccef7ff474a168f50ca79ef1f822e1
Felix Akkermans <me+code@felixakkermans.com>
2021-01-07 16:14:26 +0100

    Add profile pictures

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,6 +10,22 @@ module.exports = function(defaults) {
   });
 
   app.import('public/images/background.jpg');
-  
+  app.import('public/images/profile-pictures/arjan-de-kater.jpg');
+  app.import('public/images/profile-pictures/berend-koot.jpg');
+  app.import('public/images/profile-pictures/christ-akkermans.jpg');
+  app.import('public/images/profile-pictures/felix-akkermans.jpg');
+  app.import('public/images/profile-pictures/jan-van-willigen.jpg');
+  app.import('public/images/profile-pictures/joline-galjaard.jpg');
+  app.import('public/images/profile-pictures/jorgis-theodoulou.jpg');
+  app.import('public/images/profile-pictures/lisette-kloosterboer.jpg');
+  app.import('public/images/profile-pictures/marlies-petter.jpg');
+  app.import('public/images/profile-pictures/mitchell-vlaar.jpg');
+  app.import('public/images/profile-pictures/pim-vree.jpg');
+  app.import('public/images/profile-pictures/samuel-dullaart.jpg');
+  app.import('public/images/profile-pictures/schelte-van-de-horst.jpg');
+  app.import('public/images/profile-pictures/sjoerd-benning.jpg');
+  app.import('public/images/profile-pictures/steffan-norberhuis.jpg');
+  app.import('public/images/profile-pictures/willemijn-dreth.jpg');
+
   return app.toTree();
 };
